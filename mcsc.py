afb8a6b297e8701d57554a7b3d22c2f3ecf36938
Michael Cotner <mjc5548@rentcomp01.ccbb.utexas.edu>
2024-04-24 11:01:14 -0500

    version checking

--- mcsc.py
+++ mcsc.py
@@ -20,6 +20,11 @@ def _get_version():
     print('Failed to get version')
     return None
 
+ref_version = _get_version()
+if ref_version != VERSION and ref_version is not None:
+    print('Your mcsc version is', VERSION, 'but version', ref_version, 'exists on github.')
+    print('You can update your mcsc by running:')
+    print('\t', 'wget -N -O mcsc.py https://raw.githubusercontent.com/michaelcotner/mcsc/main/mcsc.py')
 
 def test1():
     return 'test1'
