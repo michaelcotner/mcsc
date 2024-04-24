54357f0fa5a01b0851e4afc86f5c0c566ffa62f3
Michael Cotner <mjc5548@rentcomp01.ccbb.utexas.edu>
2024-04-24 10:49:30 -0500

    fixed _get_version

--- mcsc.py
+++ mcsc.py
@@ -2,17 +2,16 @@ VERSION = '0.1'
 
 import urllib.request
 
-def _get_version_from_github():
-    # URL of the raw Python file on GitHub
-    github_url = 'https://raw.githubusercontent.com/michaelcotner/mcsc/main/mcsc.py'
+def _get_version():
+    # hosted file to compare to
+    url = 'https://raw.githubusercontent.com/michaelcotner/mcsc/main/mcsc.py'
 
-    # Fetch the raw content of the Python file
     try:
-        with urllib.request.urlopen(github_url) as response:
-            # Extract the value of the VERSION variable from the first line
+        with urllib.request.urlopen(url) as response:
+            # get version from the first line
             file_head = response.readline().decode('utf-8')
             if file_head.startswith('VERSION = '):
-                version = file_head[len('VERSION = '):].strip()
+                version = file_head[len('VERSION = '):].strip('\'')
                 return version
     except Exception as e:
         print('Failed to get version:', e)
